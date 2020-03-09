1afbc0d928d4eb9a98b6c0f8fb6aedf655f04706
Terri Grierson <tmgrierson@googlemail.com>
2020-03-09 17:53:38 +0000

    Correcting errors

--- main.js
+++ main.js
@@ -1,22 +1,28 @@
 var map;
 
 function initMap(){
+    // Map options
     var options = {
-        center:{lat:53.5500,lng:2.4333},
+        center:{lat:53.5500,lng:-2.4333},
         zoom: 2,
         mapTypedId: 'terrain'
     };
      
     //  New map
     var map = new
-    map = new google.maps.Map(document.getElementById('map'), options);
-
+    google.maps.Map(document.getElementById('map'), options);
+    
     // Listen for click on map
+    google.maps.event.addListener(map, 'click', 
+    function(event){
+        // Add marker
+        addMarker({coords:event.latLng});
+    });
 
     /*
     // Add marker
     var marker = new google.maps.Marker({
-          position:{lat:36.3932, lng:25.4615},
+          position:{lat:36.3932, lng:-25.4615},
           map: map,
           icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
         });
@@ -33,21 +39,21 @@ function initMap(){
     // Array of markers
     var markers = [
         {
-        coords:{lat:53.5500, lng:2.4333},
+        coords:{lat:53.5500, lng:-2.4333},
         iconImage:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
         content: '<h1>Lynn MA</h1>'
     },
     {
-        coords:{lat:45.4408,lng:12.3155},
+        coords:{lat:45.4408,lng:-12.3155},
         content: '<h1>Venice</h1>'
     },
     {
-        coords:{lat:52.3667,lng:4.8945}
+        coords:{lat:52.3667,lng:-4.8945}
     }
     ];
 
     // Loop through markers
-    for(var i = 0;i < markers.length; i++){
+    for(var i = 0;i < markers.length;i++){
         // Add marker
         addMarker(marker[i]);
     }
@@ -55,12 +61,12 @@ function initMap(){
     // Add Marker Function
     function addMarker(props){
         var marker = new google.maps.Marker({
-          position:coords,
+          position:props.coords,
           map: map,
           //icon: props.iconImage
         });
 
-        // Check for customicon
+        // Check for custom icon
         if(props.iconImage){
             // Set icon image
             marker.setIcon(props.iconImage);
@@ -78,4 +84,3 @@ function initMap(){
         }
     }
 }
-

e64bc9e19aa1d0aa190bd7d09732307205b7425a
Terri Grierson <tmgrierson@googlemail.com>
2020-04-09 12:07:17 +0000

    adding text annimation

--- main.js
+++ main.js
@@ -32,6 +32,20 @@ tl.fromTo(
     "-=1.2"
 )
 
+// Text annimation //
+
+function scrollAppear(){
+    var introText = document.querySelector('.intro-text');
+    var introPosition = introText.getBoundingClientRect().top;
+    var screenPosition = window.innerHeight;
+
+    if(introPosition < screenPosition){
+        introText.classList.add('intro-appear');
+    }
+}
+
+scrollAppear();
+
 // Map //
 
 function initMap(){

db7d79c82886c2b31bd5cea5ea0836325e89e83c
Terri Grierson <tmgrierson@googlemail.com>
2020-04-29 11:45:20 +0000

    adding to map

--- main.js
+++ main.js
@@ -62,7 +62,80 @@ section2.addEventListener('click', function(){
     smoothScroll('.section1', 1000);
 });
 
-// Map //
+// Map Newest //
+
+function initMap(){
+    let location = new Object();
+    navigator.geolocation.getCurrentPosition(function(pos){
+        location.lat = pos.coords.latitude;
+        location.long = pos.coords.longitude;
+        map = new google.maps.Map(document.getElementById('map'),{
+            center: {lat: location.lat, lng: location.long },
+            zoom: 15
+        });
+        getResturants(location);
+        getAccomadation(location);
+    });
+}
+
+function getResturants(location) {
+    var rome = new google.maps.LatLng(location.lat.location.long);
+    var request = {
+        location: rome,
+        radius: '1500',
+        type: ['resturant', 'accomdation']
+    };
+    service = new google.maps.places.PlaceService(map);
+    service.nearbySerach(request, callback);
+}
+
+function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+            //console.log(results);
+            var place = results[i];
+            let price = createPrice(place.price_level);
+            let content = `<h3>${place.name}</h3>
+            <h4>${place.vicinity}</h4>
+            <p>Price: ${price}<br/>
+            Rating: $[place.rating}`;
+
+            var marker = new google.maps.Marker({
+                positon: place.geometry.location,
+                map: map,
+                title: place.name
+            });
+
+            var infoWindow = new google.maps.InfoWindow({
+                content: content
+            });
+
+            bindInfoWindow(marker, map , infowindow, content);
+            marker.setMap(map);
+        }
+    }
+}
+
+function bindInfoWindow(marker, map, infowindow, html) {
+    marker.addListener('click', function() {
+        infowindow.setContent(html);
+        infowindow.open(map, this);
+    });
+}
+
+function creatPrice(level){
+    if(level != "" && level != null ){
+        let out = "";
+        for (var x = 0; x < level; x++){
+            out += "£";
+        }
+        return out;
+    }else{
+        return "?";
+    }
+}
+
+// Map 
 
 function initMap(){
     // Map options
@@ -74,6 +147,42 @@ function initMap(){
      
     //  New map
     var map = new google.maps.Map(document.getElementById('map'), options);
+
+    // Map search
+    var input = document.getElementById('search');
+    var searchBox = new google.maps.places.SearchBox(input);
+    
+    map.addListener('bounds_changed', function() {
+        searchBox.setBounds(map.getBounds());
+    });
+
+    var markers = [];
+
+    searchBox.addListener('places_changed', function() {
+        var places = searchBox.getPlaces();
+
+        if (places.length === 0)
+            return;
+
+        markers.forEach(function (m) { m.setMap(null); });
+        markers = [];
+
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function (p) {
+            if (!p.geometry)
+                return;
+            
+        markers.push(new google.maps.Marker({
+            map: map,
+            title: p.name,
+            position: p.geometry.location
+        }));
+
+        if (p.geometry.viewport)
+            bounds.union(p.geometry.viewport);
+        else
+        });
+    });
     
     // Listen for click on map
     google.maps.event.addListener(map, 'click', 
